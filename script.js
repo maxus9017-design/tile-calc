b4715debda529638197df12009694586f5e31e9c
maxus9017-design <maxus.9017@gmail.com>
2026-04-24 21:01:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,63 +4,89 @@ function rasschitat() {
   let dlina = Number(document.getElementById("dlina-input").value);
   let cena = Number(document.getElementById("cena-input").value);
   let weight = Number(document.getElementById("weight-input").value);
-  let Sh = Number(document.getElementById("tileSh-input").value);
-  let D = Number(document.getElementById("tileD-input").value);
+  let Sh_Pl = Number(document.getElementById("tileSh_Pl-input").value);
+  let D_Pl = Number(document.getElementById("tileD_Pl-input").value);
+  let shov = parseFloat(document.getElementById("shov-input").value);
+  let tolshina = parseFloat(document.getElementById("tolshina-input").value);
+  let ploshad = 0;
+  let Rezultat = "";
+  let inputs = [
+    "shirina-input",
+    "dlina-input",
+    "tileSh_Pl-input",
+    "tileD_Pl-input",
+  ];
+  let hasError = false; // Сначала ставим что все хорошо
 
-  // 2. Проверка на пустое поле или нули
-  if (
-    shirina <= 0 ||
-    dlina <= 0 ||
-    cena <= 0 ||
-    weight <= 0 ||
-    Sh <= 0 ||
-    D <= 0
-  ) {
+  inputs.forEach((id) => {
+    let element = document.getElementById(id);
+
+    // Убираем красную рамку
+    element.classList.remove("error-input");
+
+    // Проверка на 0
+    if (Number(element.value) <= 0) {
+      element.classList.add("error-input"); // Красим поле
+      hasError = true; // Запоминаем, ошибка
+    }
+  });
+
+  // Если нашли  хотя бы одну ошибку — стоп!
+  if (hasError) {
     document.getElementById("result").innerHTML =
-      "Ошибка: введите положительные числа!";
+      "<h2 style='color: red'>Заполни поля!</h2>";
+    return;
   }
-  // 3. Проверка, что введены именно цифры
-  else if (
-    isNaN(shirina) ||
-    isNaN(dlina) ||
-    isNaN(cena) ||
-    isNaN(weight) ||
-    isNaN(Sh) ||
-    isNaN(D)
-  ) {
-    document.getElementById("result").innerHTML =
-      "Ошибка: введите корректные цифры!";
-  } else {
-    // 4. Расчеты
-    let ploshad = shirina * dlina;
-    let kley = Math.ceil(ploshad / 5); // 1 мешок на 5 квадратов, округляем в большую сторону
-    let vesWeight = ploshad * weight;
-    let itogo_deneg = ploshad * cena;
-    let ploshad_s_zapasom = ploshad * 1.1; // Добовляем 10% на подрезку
-    let ploshadPlitki = (Sh / 100) * (D / 100);
-    let vsegoShtyk = Math.ceil(ploshad_s_zapasom / ploshadPlitki); // Округляем количество плиток в большую сторону
 
-    console.log(`Площадь: ${ploshad}, Цена: ${itogo_deneg} руб.`);
-
-    // 5. Выводим готовые результаты
-    document.getElementById("result").innerHTML = `
-            <h2>Ваш расчет готов:</h2>
-            <p>Площадь без подрезки: ${ploshad.toFixed(2)} м²</p>
-            <p><b>Купить плитки (+10%): ${ploshad_s_zapasom.toFixed(2)} м²</b></p>
-            <p>Итого к оплате: ${itogo_deneg} руб.</p>
-            <p>Общий вес заказа: ${vesWeight.toFixed(1)} кг</p>
-            <p>Клея понадобится: ${kley} меш. (по 25 кг)</p>
-            <p>Всего плиток: ${vsegoShtyk} шт </p>
-        `;
+  //Проверяем и считаем площадь.
+  Rezultat = "<h2> Ваш результат готов:</h2>";
+  if (shirina > 0 && dlina > 0) {
+    ploshad = shirina * dlina;
+    Rezultat += `<p> Площадь: ${ploshad.toFixed(2)} м2 </p>`;
+    if (cena > 0) {
+      let stoimost = cena * ploshad;
+      Rezultat += `<p> Стоимость плитки: <b> ${stoimost.toLocaleString()} руб.</b></p>`;
+    }
+    if (weight > 0) {
+      let obshiy_ves_plitki = ploshad * weight;
+      Rezultat += `<p> Общий вес плитки: <b> ${obshiy_ves_plitki.toFixed(2)}кг</b></p>`;
+    }
   }
+
+  if (ploshad > 0 && Sh_Pl > 0 && D_Pl > 0) {
+    let koef = Number(document.getElementById("ukladka-select").value);
+    let ploshad_s_zapasom = ploshad * koef; //Выбор подрезки
+    let ploshad_Plitki = (Sh_Pl / 100) * (D_Pl / 100);
+    let vsego_shtyk = Math.ceil(ploshad_s_zapasom / ploshad_Plitki);
+    let meshkov = Math.ceil((ploshad_s_zapasom * 5) / 25);
+    Rezultat += `<p> Плитки (с запасом ${((koef - 1) * 100).toFixed(0)}%):<b> ${ploshad_s_zapasom.toFixed(2)} м2 </b></p>`;
+    Rezultat += `<p> Всего плиток: <b> ${vsego_shtyk} шт </b></p>`;
+    Rezultat += `<p> Плиточный клей:<b> ${meshkov} шт (по 25 кг)</b></p>`;
+  }
+  //Проверяем данные для затирки.
+  if (Sh_Pl > 0 && D_Pl > 0 && shov > 0 && tolshina > 0) {
+    let L = Sh_Pl * 10; // Переводим в мм.
+    let W = D_Pl * 10; // Переводим в мм.
+
+    let rashod_na_metr = ((L + W) * tolshina * shov * 1.6) / (L * W);
+    let obshiy_ves = rashod_na_metr * ploshad;
+    Rezultat += `<p> Расход затирки кг: <b> ${rashod_na_metr.toFixed(2)} кг</b><p>`;
+    Rezultat += `<p> Общий вес: <b> ${obshiy_ves.toFixed(2)} кг</b></p>`;
+  }
+
+  document.getElementById("result").innerHTML = Rezultat;
 }
-// Очищаем все поля ввода и сбрасываем резкльтат
+
+//Очищаем все поля ввода и сбрасываем результат
 function ochistit() {
   document.getElementById("shirina-input").value = "";
   document.getElementById("dlina-input").value = "";
   document.getElementById("cena-input").value = "";
   document.getElementById("weight-input").value = "";
-  document.getElementById("tileSh-input").value = "";
-  document.getElementById("tileD-input").value = "";
+  document.getElementById("tileSh_Pl-input").value = "";
+  document.getElementById("tileD_Pl-input").value = "";
+  document.getElementById("shov-input").value = "";
+  document.getElementById("tolshina-input").value = "";
+
   document.getElementById("result").innerHTML = "Здесь появится расчет...";
 }
